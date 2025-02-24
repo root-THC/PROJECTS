0d6f9dd59fe90c73666e74b87bfc917d93c8afc5
root-THC <riicardomoreenoi@gmail.com>
2025-02-24 09:28:43 +0100

    root-THC push my project ;)

--- root-THC_CASINO /sql/casino.sql
+++ root-THC_CASINO /sql/casino.sql
@@ -1,22 +1,14 @@
-\c template1
 DROP DATABASE IF EXISTS casino_db;
 CREATE DATABASE casino_db;
-\c casino_db
+USE casino_db;
 
 -- Tabla de usuarios
-
-CREATE SEQUENCE users_seq
-  START WITH 1
-  INCREMENT BY 1;
-
-
-
 CREATE TABLE users (
-    id INT AUTO_INCREMENT  PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     passid VARCHAR(255) NOT NULL,
-    balance INT NUMERIC (8,2),  
+    balance DECIMAL(8,2) DEFAULT 1000.00,  
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -25,18 +17,20 @@ CREATE TABLE bets (
     betting_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     game VARCHAR(50) NOT NULL,
-    amount NUMERIC (8,2)NOT NULL,
+    amount DECIMAL(8,2) NOT NULL,
     result VARCHAR(50),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+-- Tabla de transacciones
 CREATE TABLE transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    type VARCHAR(10) CHECK (type IN ('deposit', 'withdrawal', 'bet', 'win')), NOT NULL,
+    type ENUM('deposit', 'withdrawal', 'bet', 'win') NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    );
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
         
