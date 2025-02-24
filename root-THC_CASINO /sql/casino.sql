5fa557f83ee89963222e2ea277f28d82b18742b9
root-THC <riicardomoreenoi@gmail.com>
2025-02-24 09:12:45 +0100

    root-THC push my project ;)

--- root-THC_CASINO /sql/casino.sql
+++ root-THC_CASINO /sql/casino.sql
@@ -12,8 +12,8 @@ CREATE TABLE users (
     id INT  PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    balance INT DEFAULT 1000,  
+     VARCHAR(255) NOT NULL,
+    saldo INT DEFAULT 1000,  
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
