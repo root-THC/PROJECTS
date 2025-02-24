379ce7d1351fd2d4a7a2bb4b2098ae70271da92c
root-THC <riicardomoreenoi@gmail.com>
2025-02-24 09:18:24 +0100

    root-THC push my project ;)

--- root-THC_CASINO /sql/casino.sql
+++ root-THC_CASINO /sql/casino.sql
@@ -22,7 +22,7 @@ CREATE TABLE users (
 
 -- Tabla de apuestas
 CREATE TABLE apuestas (
-    table_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
     juego VARCHAR(50) NOT NULL,
     quantity NUMERIC (8,2)NOT NULL,
